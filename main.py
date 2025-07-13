f613a7536a5601ae70aa3abf5abea5bc5d5a89ae
lakustovdimka <lakustovdimka@gmail.com>
2025-07-14 01:00:34 +0500

    qwqewewew32322121

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_weather(latitude, longitude):
 
     try:
         response = requests.get(base_url, params=params)
-        response.raise_for_status() # Вызывает искdsdewsлdsdw32dewdsdsssd21wюfddffsddчение для ошибок HTTP (4xx или 5xx)
+        response.raise_for_status() # Вызывает искd21sdewsлdsdw32dewdsdsssd21wюfddffsddчение для ошибок HTTP (4xx или 5xx)
         data = response.json()
 
         # Извлечение текущей погоды
